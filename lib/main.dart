1f90c4c047494c280b2d8a40924193e775380bdb
Alexander <alex12a7x@gmail.com>
2022-05-06 17:08:53 -0300

    interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
+  
   runApp(MaterialApp(
-    title: "Anatel",
-    home: Container(color: Colors.blue,),
+    home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.black,
+      ) ,
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/anatel-logo.png",
+            width: 200,
+            ),
+            Text("clique para gerar uma frase!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 36,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            RaisedButton(
+              child: Text(
+                  "Nova Frase",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              color: Colors.blue,
+              onPressed: (){},
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
